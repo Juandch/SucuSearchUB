4fd4a0226ab750e7e0e567953dacf9eeae468031
Juan David <juandch@outlook.com>
2019-02-20 02:42:04 -0500

    Create and Read the suculents

--- app/controllers/suculents_controller.rb
+++ app/controllers/suculents_controller.rb
@@ -1,5 +1,27 @@
 class SuculentsController < ApplicationController
 
+  def allImages
+      @images = Image.all
+  end
+
   def new
+  end
+
+  def create
+    #render plain: params[:image].ispect
+    @image = Image.new image_params
+    @image.save
+
+    redirect_to @image
+  end
+
+  def show
+    @image = Image.find params[:id]
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:all)
   end
 end
